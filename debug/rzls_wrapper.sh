e550584984482dd51ade3aedff5fcded48fdb0bb
Reese Pollard <rpollard@gmail.com>
2024-09-14 12:03:55 -0700

    wrapper to rzls in orig location

--- debug/rzls_wrapper.sh
+++ debug/rzls_wrapper.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-~/.local/share/nvim/rzls/rzls "$@" 2> /Users/reesepollard/.cache/nvim/rzls_server.log
+# ~/.local/share/nvim/rzls/rzls "$@" 2> /Users/reesepollard/.cache/nvim/rzls_server.log
+/Users/reesepollard/projects/dotnet/razor/artifacts/LanguageServer/Release/net8.0/osx-arm64/rzls "$@" 2> /Users/reesepollard/.cache/nvim/rzls_server.log
+

4f6ee6f052390261a0b19726c4e1a36b79b41ccb
Reese Pollard <rpollard@gmail.com>
2024-09-19 21:34:17 -0700

    Unify the buf_registry utility for now

--- lua/cutlass/buf_registry.lua
+++ lua/cutlass/buf_registry.lua
@@ -1,25 +1,80 @@
 local debug = require("cutlass.debug")
-local proj_buf = require("cutlass.projected_buf")
+local lspconfig = require("lspconfig")
+local lsp_functions = require("cutlass.break_the_loop")
+local util = require("cutlass.util")
 local api = vim.api
 
 local M = {}
 local registry = {}
 local registry_bufname = {}
 
+-- need to be able to clear the buffer
+-- need to be able to fully replace the buffer content
+-- edit a given position
+-- take a list of positions(spans) and content and replace that span with the new content
+-- need to figure out how the versioning works and bump it when necessary idk
+
+---@class ProjectedBufState
+---@field bufname string -- bufname is the full path
+---@field parent_bufnr integer
+---@field root_dir string?
+---@field proj_html_bufnr integer?
+---@field proj_html_vers integer
+---@field proj_html_line_ending string
+---@field proj_cs_bufnr integer?
+---@field proj_cs_vers integer?
+---@field proj_cs_line_ending string
+
 ---@param parent_bufnr integer
 ---@param bufname string
 ---@param root_dir string?
----@param html_lsp_config vim.lsp.ClientConfig
----@param csharp_lsp_config vim.lsp.ClientConfig
-function M.register(parent_bufnr, bufname, root_dir)
+---@return ProjectedBufState
+local function init_state(parent_bufnr, bufname, root_dir)
+	return {
+		bufname = bufname,
+		parent_bufnr = parent_bufnr,
+		root_dir = root_dir,
+		proj_html_bufnr = nil,
+		proj_html_vers = 0,
+		proj_html_line_ending = "\r\n",
+		proj_cs_bufnr = nil,
+		proj_cs_vers = nil,
+		proj_cs_line_ending = "\r\n",
+	}
+end
+
+---@param state ProjectedBufState
+local function create_proj_buffers(state)
+	-- Create a buffer if its not created
+	if not state.proj_html_bufnr then
+		state.proj_html_bufnr = api.nvim_create_buf(true, true)
+
+		debug.log_message("html proj buffer id: " .. state.proj_html_bufnr)
+	end
+
+	-- Set the filetypes
+	api.nvim_set_option_value("filetype", "html", { buf = state.proj_html_bufnr })
+	-- Set the fileencoding to the same as the parent
+	api.nvim_set_option_value(
+		"fileencoding",
+		api.nvim_get_option_value("fileencoding", { buf = state.parent_bufnr }),
+		{ buf = state.proj_html_bufnr }
+	)
+end
+
+---@param parent_bufnr integer
+---@param bufname string
+---@param root_dir string?
+---@param handlers table
+function M.register(parent_bufnr, bufname, root_dir, handlers)
 	if registry[parent_bufnr] then
 		return
 	end
 	debug.log_message("Register bufnr: " .. parent_bufnr .. " root_dir: " .. root_dir)
 
-	local state = proj_buf.init_state(parent_bufnr, bufname, root_dir)
-	proj_buf.create_proj_buffers(state)
-	proj_buf.attach_lsps(state)
+	local state = init_state(parent_bufnr, bufname, root_dir)
+	create_proj_buffers(state)
+	util.attach_lsps(state, handlers)
 	registry[parent_bufnr] = state
 	registry_bufname[bufname] = state
 end
@@ -45,6 +100,17 @@ function M.get_by_name(bufname)
 	return registry_bufname[bufname]
 end
 
+---@param child_bufnr integer
+---@return integer?
+function M.get_parent_bufnr(child_bufnr)
+	for parent_bufnr, registry_entry in pairs(registry) do
+		if registry_entry.proj_html_bufnr == child_bufnr or registry_entry.proj_cs_bufnr == child_bufnr then
+			return parent_bufnr
+		end
+	end
+	return nil
+end
+
 ---@param bufnr integer
 function M.reset_projected_state(bufnr)
 	if not registry[bufnr] then
@@ -55,6 +121,8 @@ function M.reset_projected_state(bufnr)
 	registry[bufnr].proj_html_vers = 0
 	registry[bufnr].proj_cs_vers = 0
 end
+
+--
 
 -- TODO: handle buf rename in the registry - this should be attached to an autocommand
 
